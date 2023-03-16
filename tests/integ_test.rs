dc79a60ec804230cec8b44028b0b3da993546a46
xiema <maxprincipe@yahoo.com>
2023-03-17 02:15:34 +0800

    add integ tests

---
+++ tests/integ_test.rs
@@ -0,0 +1,41 @@
+use rs_compile::parser::*;
+use std::fs;
+
+
+#[allow(unused_variables)]
+#[test]
+fn integ_test_ll() {
+    let (mut tok, gram) = rs_compile::define_lang_ll();
+    
+    let asm = fs::read_to_string("tests/input/sample_grammar_ll.txt").unwrap();
+    let tokens = tok.tokenize(asm.as_str()).unwrap();
+    let parser = ParserLL::new(&gram);
+    let ast = parser.parse(&tokens, 0).unwrap();
+    
+    let asm = fs::read_to_string("tests/input/sample_grammar_lr.txt").unwrap();
+    let tokens = tok.tokenize(asm.as_str()).unwrap();
+    let parser = ParserLL::new(&gram);
+    let ast = parser.parse(&tokens, 0).unwrap();
+    // display_ast(0, &ast, &gram, 0);
+
+    // parser.display_parse_table();
+}
+
+#[allow(unused_variables)]
+#[test]
+fn integ_test_lr() {
+    let (mut tok, gram) = rs_compile::define_lang_lr();
+
+    let asm = fs::read_to_string("tests/input/sample_grammar_lr.txt").unwrap();
+    let tokens = tok.tokenize(asm.as_str()).unwrap();
+    let parser = ParserLR::new(&gram);
+    let ast = parser.parse(&tokens, 0).unwrap();
+
+    let asm = fs::read_to_string("tests/input/sample_grammar_ll.txt").unwrap();
+    let tokens = tok.tokenize(asm.as_str()).unwrap();
+    let parser = ParserLR::new(&gram);
+    let ast = parser.parse(&tokens, 0).unwrap();
+    // display_ast(0, &ast, &gram, 0);
+
+    // parser.display_parse_table();
+}
